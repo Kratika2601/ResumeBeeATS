c59d3fc3b0f34df8a4d372a644bb5d78e72077c0
Nikhilesh Gautam <103520420+NICK0659@users.noreply.github.com>
2024-05-13 07:38:52 +0530

    effects

--- src/HOME.js
+++ src/HOME.js
@@ -4,3 +4,9 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true
 });
+
+//sheryjs
+Shery.imageEffect(".image", {
+    style: 6,
+    debug: true,
+  });
